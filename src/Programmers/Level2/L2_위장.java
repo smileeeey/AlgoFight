d815fb08a7c835c26e99ece0cac16544c76e6e3e
sumin416 <sumin416@naver.com>
2021-01-05 23:40:22 +0900

    20210105

--- src/Programmers/Level2/L2_위장.java
+++ src/Programmers/Level2/L2_위장.java
@@ -1,17 +1,37 @@
 package Programmers.Level2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class L2_위장 {
 
 	public static void main(String[] args) {
-		System.out.println(new L2_위장().solution(new String[][] {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}}));
+		System.out.println(new L2_위장().solution(new String[][] {{"crow_mask", "face"}, {"blue_sunglasses", "face"}, {"smoky_makeup", "face"}}));
 	}
 
-	private int solution(String[][] strings) {
-		int answer = 0;
+	private int solution(String[][] clothes) {
+		int answer = 1;
 		
+		List<String> types = new ArrayList<>();
+		int[] counts = new int[30];
 		
+		loop:
+		for(String[] str : clothes) {
+			for (int i = 0; i < types.size(); i++) {
+				if(types.get(i).equals(str[1])) {
+					counts[i]++;
+					continue loop;
+				}
+			}
+			counts[types.size()] = 1;
+			types.add(str[1]);
+		}
 		
-		return answer;
+		for (int i = 0; i < types.size(); i++) {
+			answer *= (counts[i]+1);
+		}
+		
+		return answer-1;
 	}
 
 }
