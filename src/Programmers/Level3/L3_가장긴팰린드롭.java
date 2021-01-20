ab0b7ff77ddd97f4ec665763d2e5b8c97293958f
sumin416 <sumin416@naver.com>
2021-01-21 00:13:12 +0900

    update 가장긴팰린드롭

--- src/Programmers/Level3/L3_가장긴팰린드롭.java
+++ src/Programmers/Level3/L3_가장긴팰린드롭.java
@@ -3,11 +3,11 @@ package Programmers.Level3;
 public class L3_가장긴팰린드롭 {
 
 	public static void main(String[] args) {
-		System.out.println(new L3_가장긴팰린드롭().solution("abacde"));
+		System.out.println(new L3_가장긴팰린드롭().solution("a"));
 	}
 
-	private int solution(String s) {
-		int answer = 0;
+	public int solution(String s) {
+		int answer = 1;
 		int len = s.length();
 		char[] chs = s.toCharArray();
 		
@@ -32,6 +32,29 @@ public class L3_가장긴팰린드롭 {
 			}
 		}
 		
+		for (int i = 0; i < len-1; i++) {
+			
+			
+			if(chs[i]!=chs[i+1]) {
+				continue;
+			}
+			int cnt = 2;
+			for (int j = 1; j < len; j++) {
+				int left = i-j;
+				int right = i+1+j;
+				
+				if(left<0 || right>=len) {
+					answer = Math.max(answer, cnt);
+					break;
+				}
+				
+				if(chs[left]==chs[right])	cnt+=2;
+				else {
+					answer = Math.max(answer, cnt);
+					break;
+				}
+			}
+		}
 		
 		return answer;
 	}
